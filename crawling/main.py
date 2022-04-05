d4a4c33d6796f8297423f2baa766293919105380
devLupin <luckyquit49@gmail.com>
2022-04-05 20:29:49 +0900

    crawling, detection integration

---
+++ crawling/main.py
@@ -0,0 +1,126 @@
+# https://pgh268400.tistory.com/260
+
+import requests
+from bs4 import BeautifulSoup
+import time
+import os
+from os.path import getsize
+import csv
+ 
+def image_download(title, link, log_csv):
+ 
+    # 헤더 설정 (필요한 대부분의 정보 제공 -> Bot Block 회피)
+    headers = {
+    "Connection" : "keep-alive",
+    "Cache-Control" : "max-age=0",
+    "sec-ch-ua-mobile" : "?0",
+    "DNT" : "1",
+    "Upgrade-Insecure-Requests" : "1",
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Sec-Fetch-Site" : "none",
+    "Sec-Fetch-Mode" : "navigate",
+    "Sec-Fetch-User" : "?1",
+    "Sec-Fetch-Dest" : "document",
+    "Accept-Encoding" : "gzip, deflate, br",
+    "Accept-Language" : "ko-KR,ko;q=0.9"
+    }
+ 
+    res = requests.get(link, headers=headers)
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+ 
+    # 아래 이미지 다운로드 받는 곳에서 시작
+    image_download_contents = soup.select("div.appending_file_box ul li")
+    for li in image_download_contents:
+        img_tag = li.find('a', href=True)
+        img_url = img_tag['href']
+ 
+        file_ext = img_url.split('.')[-1]
+        #저장될 파일명
+        savename = img_url.split("no=")[2]
+        headers['Referer'] = link
+        response = requests.get(img_url, headers=headers)
+        
+        path = f"Image/{savename}"
+        file_size = len(response.content)
+ 
+        if os.path.isfile(path) and getsize(path) != file_size:
+            if getsize(path) != file_size:
+                file = open(path + "[1]", "wb")
+                file.write(response.content)
+                file.close()
+                
+                with open(log_csv, 'a') as f:
+                    wr = csv.writer(f)
+                    wr.writerow([title, link, savename])
+            else:
+                pass
+
+        else:
+            file = open(path , "wb")
+            file.write(response.content)
+            file.close()
+            
+            with open(log_csv, 'a') as f:
+                wr = csv.writer(f)
+                wr.writerow([title, link, savename])
+ 
+def image_check(text):
+    text = str(text)
+    if "icon_pic" in text:
+        return True
+    else:
+        return False
+
+        
+def main(BASE_URL, log_csv):
+ 
+    # 헤더 설정 (필요한 대부분의 정보 제공 -> Bot Block 회피)
+    headers = {
+    "Connection" : "keep-alive",
+    "Cache-Control" : "max-age=0",
+    "sec-ch-ua-mobile" : "?0",
+    "DNT" : "1",
+    "Upgrade-Insecure-Requests" : "1",
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Sec-Fetch-Site" : "none",
+    "Sec-Fetch-Mode" : "navigate",
+    "Sec-Fetch-User" : "?1",
+    "Sec-Fetch-Dest" : "document",
+    "Accept-Encoding" : "gzip, deflate, br",
+    "Accept-Language" : "ko-KR,ko;q=0.9"
+ 
+              }
+ 
+    res = requests.get(BASE_URL, headers=headers)
+ 
+    if res.status_code == 200:
+        html = res.text
+        soup = BeautifulSoup(html, 'html.parser')
+        #print(soup)
+        doc = soup.select("td.gall_tit > a:nth-child(1)")
+        for i in range(4,len(doc)): #공지사항 거르고 시작 (인덱스 뒤부터)
+            link = "https://gall.dcinside.com" + doc[i].get("href") #글 링크
+            title = doc[i].text.strip() # 제목
+            image_insert = image_check(doc[i]) #이미지 포함여부
+            
+            if(image_insert == True): #이미지 포함시
+                image_download(title, link, log_csv) #이미지 다운로드하기
+            break #바로 break해서 첫글만 가져옴
+    time.sleep(2)
+    
+if __name__ == '__main__':
+    log_csv = 'log.csv'
+    if not os.path.exists(log_csv):
+        with open(log_csv, 'w') as f:
+            wr = csv.writer(f)
+            wr.writerow(['title', 'link', 'file name'])
+    
+    BASE_URL = "https://gall.dcinside.com/board/lists/?id=w_entertainer"
+    
+    print(f"[*] Image crawling start")
+    
+    while(True):
+        main(BASE_URL, log_csv)
---
+++ detection/main.py
@@ -0,0 +1,137 @@
+from __future__ import annotations
+import os, errno
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import numpy as np
+
+import tensorflow as tf
+from utils import label_map_util
+import os
+import cv2
+
+from PIL import Image
+
+from silence_tensorflow import silence_tensorflow
+silence_tensorflow()
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = './model/frozen_inference_graph_face.pb'
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = './protos/face_label_map.pbtxt'
+NUM_CLASSES = 2
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+class TensoflowFaceDector(object):
+    def __init__(self, PATH_TO_CKPT):
+        """Tensorflow detector
+        """
+
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.compat.v1.GraphDef()
+            with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+        with self.detection_graph.as_default():
+            config = tf.compat.v1.ConfigProto()
+            config.gpu_options.allow_growth = True
+            self.sess = tf.compat.v1.Session(
+                graph=self.detection_graph, config=config)
+            self.windowNotSet = True
+
+    def run(self, image):
+        """image: bgr image
+        return (boxes, scores, classes, num_detections)
+        """
+
+        image_np = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # the array based representation of the image will be used later in order to prepare the
+        # result image with boxes and labels on it.
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        image_tensor = self.detection_graph.get_tensor_by_name(
+            'image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = self.detection_graph.get_tensor_by_name(
+            'detection_classes:0')
+        num_detections = self.detection_graph.get_tensor_by_name(
+            'num_detections:0')
+        # Actual detection.
+        # start_time = time.time()
+        (boxes, scores, classes, num_detections) = self.sess.run(
+            [boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+        # elapsed_time = time.time() - start_time
+        # print('inference time cost: {}'.format(elapsed_time))
+
+        return (boxes, scores, classes, num_detections)
+
+
+def silentRemove(file):
+    try:
+        os.remove(file)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+
+def main(db_path):
+    tDetector = TensoflowFaceDector(PATH_TO_CKPT)
+    
+    for i in os.listdir(db_path):
+        path = os.path.join(db_path, i)
+        img = cv2.imread(path, cv2.IMREAD_COLOR)
+        
+        if img is None: 
+            silentRemove(path)
+            continue
+        
+        [h, w] = img.shape[:2]
+            
+        (boxes, scores, classes, num_detections) = tDetector.run(img)
+        
+        box = np.squeeze(boxes)
+        score = np.squeeze(scores)
+        
+        if (score[0] > 0.90):   # if detect face
+            ymin, xmin, ymax, xmax = box[0]
+
+            # pil_image = Image.fromarray(np.uint8(image)).convert('RGB')
+            temp = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            pil_image = Image.fromarray(temp)
+
+
+            (left, right, top, bottom) = (xmin * w, xmax * w,
+                                          ymin * h, ymax * h)
+
+            cropped = pil_image.crop((left, top, right, bottom))
+            cropped = cropped.resize((112, 112), Image.ANTIALIAS)
+            # cropped.show()
+            cropped.save(os.path.join('crop_img', i), format='PNG', quality=100)
+            
+        silentRemove(path)
+
+if __name__ == '__main__':
+    
+    
+    db_path = '../crawling/Image'
+    
+    print(f"[*] Detection and RoI cropping !")
+    
+    while(True):
+        num_imgs = len(os.listdir(db_path))
+        
+        if num_imgs < 20:
+            continue
+        
+        print(f"[*] {num_imgs} images RoI cropping")
+        main(db_path)
+        print('[*] done !')
